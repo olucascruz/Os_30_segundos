448df5d2888bb0ce29c56db9a14f8ec45427b526
LucasCruz <lca.snf21@uea.edu.br>
2022-07-05 00:59:49 -0400

    add download text

--- popup.js
+++ popup.js
@@ -1,13 +1,18 @@
 let buttonAdd = document.getElementById("Add");
 let register = document.getElementById("register");
+let filename;
+
 buttonAdd.addEventListener("click", () => {
-    
-  if(register.classList.contains("close")){
-      buttonAdd.classList.add("close")
-      register.classList.remove("close")
-      timer();   
-    }
-  });
+  filename = document.getElementById("filename").value;
+
+  if(filename){    
+    if(register.classList.contains("close")){
+        buttonAdd.classList.add("close")
+        register.classList.remove("close")
+        timer();   
+      }
+  }
+});
 
 let writingTime = 30;
 const timer = () => {
@@ -21,13 +26,34 @@ const timer = () => {
       writingTime--;
 
       if(writingTime < 0){
+          const content = document.getElementById("text").value;
           buttonAdd.classList.remove("close")
           register.classList.add("close")
           document.getElementById('head').removeChild(pTimer);
+          downloadText(filename, content)
           return clearInterval(time);
       }
 
     }())
     },1000)
+
+}
+
+
+function downloadText(filename, content){
+   const element = document.createElement('a');
+
+   const blob = new Blob([content], {
+    type:'plain/text' 
+   })
+   const fileUrl = URL.createObjectURL(blob);
+   element.setAttribute('href', fileUrl);
+   element.setAttribute('download', filename+'.txt');
+   element.style.display = 'none';
+
+   document.body.appendChild(element);
+   element.click();
+
+   document.body.removeChild(element);
 
 }
